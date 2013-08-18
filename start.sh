d59483d1b2392c459f26466a813c0e225ebd9152
Evgeni Makarov <evgeni.makarov@gmail.com>
2013-08-18 23:11:52 +0400

    virtual environment fix

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # run this as the djangorunner user!
 
-VIRTUALENV_DIR=/home/makarov/envs/sciencetalks
+VIRTUALENV_DIR=/home/makarov/envs/ovarenik
 source $VIRTUALENV_DIR/bin/activate
 cd /home/makarov/sciencetalks/
 python manage.py runfcgi method=threaded debug=False host=127.0.0.1 port=8102 pidfile=/home/makarov/runfiles/sciencetalks.pid
